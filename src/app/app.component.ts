56d6aa5bf30be5076dbf14c998b7d6b0d4a80a73
Roman <romangnus.rg@gmail.com>
2019-02-14 10:51:51 +0200

    add cars-action for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,12 @@ export class AppComponent implements OnInit {
   cars: Car[];
 
   ngOnInit() {
-    this.cars = [
-      {name: 'Ford', date: '09.02.19', model: 'Focused', isSold: false, id: 1},
-      {name: 'Audi', date: '09.05.18', model: 'A4', isSold: false, id: 2},
-      {name: 'Ford', date: '05.02.18', model: 'Focused', isSold: true, id: 3},
-      // WTF why isn`t working following code?
-      // new Car('Ford', '09.02.19', 'Focused', false, 1),
-      // new Car('Audi', '09.05.18', 'A4', false, 2),
-      // new Car('Ford', '09.02.19', 'Focused', false, 1),
-    ];
+    this.cars = [];
+    // this.cars = [
+    //   {name: 'Ford', date: '09.02.19', model: 'Focused', isSold: false, id: 1},
+    //   {name: 'Audi', date: '09.05.18', model: 'A4', isSold: false, id: 2},
+    //   {name: 'Ford', date: '05.02.18', model: 'Focused', isSold: true, id: 3},
+    // ];
   }
 
   onAdd(car: Car) {
--- src/app/redux/cars.reducer.ts
+++ src/app/redux/cars.reducer.ts
@@ -1,7 +1,11 @@
+import { Action } from '@ngrx/store';
+
 const initialState = {
-  cars: [],
+  cars: [{name: 'Ford', date: '09.02.19', model: 'Focused', isSold: false, id: 1},
+  {name: 'Audi', date: '09.05.18', model: 'A4', isSold: false, id: 2},
+  {name: 'Ford', date: '05.02.18', model: 'Focused', isSold: true, id: 3}]
 };
 
-export function carsReducer(state = {}, action) {
+export function carsReducer(state = initialState, action: Action) {
 
 }
